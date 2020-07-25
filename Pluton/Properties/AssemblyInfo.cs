cfcd8e579eb2533df60df4ab04988b5522e05cd3
Relickw-dev <59470150+Relickw-dev@users.noreply.github.com>
2020-07-25 17:36:40 +0300

    Initial commit

---
+++ Pluton/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Pluton")]
+[assembly: AssemblyDescription("Manage Windows security systems, administrative tools and simplify some functions.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Radu Andrei")]
+[assembly: AssemblyProduct("Pluton")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("Radu Andrei")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("96884737-cff1-4a9b-8653-2c9ab5402684")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: NeutralResourcesLanguage("en")]
